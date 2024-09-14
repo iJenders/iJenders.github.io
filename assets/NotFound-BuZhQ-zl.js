25ac3d88d49688d1eaba334bc97519696504c86a
iJenders <114449109+iJenders@users.noreply.github.com>
2024-09-14 18:01:47 -0400

    Add files via upload

---
+++ assets/NotFound-BuZhQ-zl.js
@@ -0,0 +1,1 @@
+import{s as r,r as a,f as l,g as e,v as n,t as u,F as p,e as d}from"./index-Bc7aKEuB.js";const i={__name:"NotFound",setup(f){const o=r(),s=a(o.path);return(m,t)=>(d(),l(p,null,[t[2]||(t[2]=e("h1",null,"404",-1)),e("h2",null,[t[0]||(t[0]=n("Page not found: ")),t[1]||(t[1]=e("br",null,null,-1)),n(u(s.value),1)])],64))}};export{i as default};
